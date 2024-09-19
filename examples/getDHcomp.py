907a32f0180071afaaebde547754115c1bab3e9e
Liwenhao <2753661300@qq.com>
2024-09-19 17:31:13 +0800

    重来一次

--- examples/getDHcomp.py
+++
@@ -1,6 +0,0 @@
-from fairino import Robot
-
-# 与机器人控制器建立连接，连接成功返回一个机器人对象
-robot = Robot.RPC('192.168.58.2')
-error = robot.GetDHCompensation()
-print(error)
--- examples/robot_point_table_upload.py
+++
@@ -1,6 +0,0 @@
-from fairino import Robot
-
-# 与机器人控制器建立连接，连接成功返回一个机器人对象
-robot = Robot.RPC('192.168.58.2')
-error = robot.PointTableUpLoad("/home/fr/RD06/SDK1/test/point_table_a.db")
-print("PointTableUpLoad错误码:",error)
